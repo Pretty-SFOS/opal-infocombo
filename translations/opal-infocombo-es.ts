bc5fe1feca3ae78e0f011e5dcb2f8f4d86cc30cd
Weblate <noreply@weblate.org>
2023-06-09 23:41:52 +0200

    Added translation using Weblate (Spanish)

---
+++ translations/opal-infocombo-es.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+<context>
+    <name>Opal.InfoCombo</name>
+    <message>
+        <location filename="../Opal/InfoCombo/private/InfoComboPage.qml" line="32"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/InfoCombo/private/InfoComboPage.qml" line="66"/>
+        <source>Option</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
