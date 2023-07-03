b34ebc02a6c523d9b3cce702e4ed178876c6f81d
Reza Almanda <rezaalmanda27@gmail.com>
2023-07-03 06:51:50 +0200

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Co-authored-by: Reza Almanda <rezaalmanda27@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-infocombo/id/
    Translation: Opal for SailfishOS/Module: Opal.InfoCombo

--- translations/opal-infocombo-id.ts
+++ translations/opal-infocombo-id.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../Opal/InfoCombo/private/InfoComboPage.qml" line="34"/>
         <source>Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Detil</translation>
     </message>
     <message>
         <location filename="../Opal/InfoCombo/private/InfoComboPage.qml" line="74"/>
         <source>Option</source>
-        <translation type="unfinished"></translation>
+        <translation>Opsi</translation>
     </message>
 </context>
 </TS>
